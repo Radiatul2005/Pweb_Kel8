b045767a728fb8a1a4e5e83c9ead2f9350e45f08
Radiatul2005 <radiatulmutmainnah64@gmail.com>
2024-05-14 14:13:02 +0700

    app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var editRouter = require('./routes/edit')
-var loginRouter = require('./routes/loginRouter');
+// Import routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const editRouter = require('./routes/edit');
+const loginRouter = require('./routes/loginRouter'); // Menambahkan import untuk loginRouter
 
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,29 +22,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/editprofile', editRouter);
-app.use('/login', loginRouter);
+app.use('/login', loginRouter); // Menambahkan rute untuk halaman login
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-const port = 3000; // Port yang akan digunakan (dalam kasus ini, port 3000)
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+const PORT = process.env.PORT || 3000; // Menentukan port aplikasi (default: 3000)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
